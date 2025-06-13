22dcdcc9eb2f3dd36df6aa0908782ee066ae41b8
guilherme2000925 <guilherme2000925@WSFAR37KT7G>
2025-06-13 11:40:26 -0300

    Adicionar arquivos de projeto.

---
+++ GeradorStringAlfanumerico.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace Desafio_01
+{
+    public class GeradorStringAlfanumerico
+    {
+        public string GerarStringAlfanmerico(int comprimento)
+        {
+            string caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            Random random = new();
+            StringBuilder alfanumerico = new StringBuilder(comprimento);
+
+
+            for (int i = 0; i < comprimento; i++)
+            {
+                int indiceAleatorio = random.Next(0, caracteres.Length);
+                alfanumerico.Append(caracteres[indiceAleatorio]);
+            }
+            return alfanumerico.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+
+namespace Desafio_01
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            var geradorAlfanumerico = new GeradorStringAlfanumerico();
+            int comprimentoDesejado = 6;
+            string alfanumericoAleatoriaA = geradorAlfanumerico.GerarStringAlfanmerico(comprimentoDesejado);
+            string alfanumericoAleatoriaB = geradorAlfanumerico.GerarStringAlfanmerico(comprimentoDesejado);
+            string alfanumericoAleatoriaC = geradorAlfanumerico.GerarStringAlfanmerico(comprimentoDesejado);
+            string alfanumericoAleatoriaD = geradorAlfanumerico.GerarStringAlfanmerico(comprimentoDesejado);
+
+            var parametrosJSON = new ParametrosJSON
+            {
+                A = alfanumericoAleatoriaA,
+                B = alfanumericoAleatoriaB,
+                C = alfanumericoAleatoriaC,
+                D = alfanumericoAleatoriaD
+            };
+
+            string jsonString = JsonSerializer.Serialize<ParametrosJSON>(parametrosJSON);
+
+            Console.WriteLine(jsonString);
+
+        }
+    }
+}
