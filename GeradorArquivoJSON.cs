ebd7f6ec0f9703478fa0ea73e9333ab5d4c1bd6d
Guilherme Campos <guilhermev272@gmail.com>
2025-06-26 15:15:41 -0300

    Update GeradorArquivoJSON.cs

--- GeradorArquivoJSON.cs
+++ GeradorArquivoJSON.cs
@@ -1,61 +1,64 @@
-﻿using System.Text.Json;
+using System.Text.Json;
 
 namespace Desafio_01
 {
     public class GeradorArquivoJSON
     {
-        public void GerarArquivoJson(IEnumerable<ParametrosJSON> parametrosJSON, string pastaDestino, int tamanhoArquivoDesejado)
+        public void GerarArquivoJson(string pastaDestino, int tamanhoArquivoDesejado)
         {
-            double limiteMaximoEmMB = 400.00;
+            GeradorStringAlfanumerico geradorStringAlfanumerico = new();
+
+            double limiteMaximoEmMB = 20000.00;
             double limiteComTolerancia = limiteMaximoEmMB + (limiteMaximoEmMB / 100);
+            int loopEquivalente1MB = 13500;
+            int quantidadeLoop = tamanhoArquivoDesejado * loopEquivalente1MB;
             int quantidadeObjetos = 0;
 
             if (File.Exists(pastaDestino) && tamanhoArquivoDesejado < limiteComTolerancia)
             {
                 File.Delete(pastaDestino);
-                using (FileStream fileStream = new(pastaDestino, FileMode.Create))
-                {
-                    using var writer = new Utf8JsonWriter(fileStream, new JsonWriterOptions { Indented = true });
-
-                    writer.WriteStartArray();
-                    foreach (var parametros in parametrosJSON)
-                    {
-                        writer.WriteStartObject();
-                        writer.WriteString("A", parametros.A);
-                        writer.WriteString("B", parametros.B);
-                        writer.WriteString("C", parametros.C);
-                        writer.WriteString("D", parametros.D);
-                        writer.WriteEndObject();
-                        quantidadeObjetos += 4;
-                    }
-                    writer.WriteEndArray();
-                }
+                EscreverArquivo(pastaDestino, geradorStringAlfanumerico, quantidadeLoop);
                 Console.WriteLine("\nArquivo JSON atualizado.");
                 Console.WriteLine($"\nQuatidade de objetos criados: {quantidadeObjetos}");
             }
             else if (tamanhoArquivoDesejado < limiteComTolerancia)
             {
-                using (FileStream fileStream = new(pastaDestino, FileMode.Create))
-                {
-                    using var writer = new Utf8JsonWriter(fileStream, new JsonWriterOptions { Indented = true });
-
-                    writer.WriteStartArray();
-                    foreach (var parametros in parametrosJSON)
-                    {
-                        writer.WriteStartObject();
-                        writer.WriteString("A", parametros.A);
-                        writer.WriteString("B", parametros.B);
-                        writer.WriteString("C", parametros.C);
-                        writer.WriteString("D", parametros.D);
-                        writer.WriteEndObject();
-                        quantidadeObjetos += 4;
-                    }
-                    writer.WriteEndArray();
-                }
+                EscreverArquivo(pastaDestino, geradorStringAlfanumerico, quantidadeLoop);
                 Console.WriteLine("\nArquivo JSON criado.");
                 Console.WriteLine($"\nQuatidade de objetos criados: {quantidadeObjetos}");
             }
         }
+
+        private void EscreverArquivo(string pastaDestino, GeradorStringAlfanumerico geradorStringAlfanumerico, int quantidadeLoop)
+        {
+            try
+            {
+                using StreamWriter writer = new(pastaDestino);
+                GerarLinha(geradorStringAlfanumerico);
+                for (int i = 0; i < quantidadeLoop; i++)
+                {
+                    writer.WriteLine(GerarLinha(geradorStringAlfanumerico) + (i < quantidadeLoop - 1 ? "," : ""));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"\nException: {ex.Message}");
+            }
+        }
+
+        private string GerarLinha(GeradorStringAlfanumerico geradorStringAlfanumerico)
+        {
+            JsonSerializerOptions identacao = new() { WriteIndented = true };
+            var parametros = new ParametrosJSON
+            {
+                A = geradorStringAlfanumerico.GetAlfanumericoAleatoria(),
+                B = geradorStringAlfanumerico.GetAlfanumericoAleatoria(),
+                C = geradorStringAlfanumerico.GetAlfanumericoAleatoria(),
+                D = geradorStringAlfanumerico.GetAlfanumericoAleatoria()
+            };
+
+            var linha = JsonSerializer.Serialize(parametros, identacao);
+            return linha;
+        }
     }
 }
-
